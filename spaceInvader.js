77f736ed7a56e278afa85ae788f6d136f801bd92
Kiara Geuten <kiara.geuten@student.ehb.be>
2024-09-27 01:34:29 +0200

    blck bckgrnd

--- spaceInvader.js
+++ spaceInvader.js
@@ -7,6 +7,11 @@ function spaceinvader() {
     canvas.height = window.innerHeight;
     let context = canvas.getContext("2d");
 
+context.fillStyle= "black"
+context.beginPath();
+context.fillRect(0,0,300,300);
+context.closePath();
+
 context.fillStyle= "#C17AD0"
 context.beginPath();
 context.fillRect(75,25,50,50);
@@ -16,6 +21,7 @@ context.fillRect(125,175,50,50);
 context.fillRect(175,125,50,50);
 context.fillRect(175,25,50,50);
 context.fillRect(225,75,50,200);
+context.closePath();
 context.fill();
 
 }
