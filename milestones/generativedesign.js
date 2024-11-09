3ed494e9dc343e030fd045722ba26d05d6b39838
Kiara Geuten <kiara.geuten@student.ehb.be>
2024-11-09 16:23:51 +0100

    kleruen fix

--- milestones/generativedesign.js
+++ milestones/generativedesign.js
@@ -1,19 +1,25 @@
 import context from "../scripts/context.js";
 import * as Utils from "../scripts/utils.js";
 
-// context.fillRect(0, 0, 50, 50);
-// context.fillStyle = "red";
-// context.fillRect(50, 0, 50, 50);
-// context.fillRect(0, 50, 50, 50);
-// context.fillStyle = "yellow";
-// context.fillRect(100, 0, 50, 50);
-// context.fillRect(0, 100, 50, 50);
-// context.fillRect(50, 50, 50, 50);
+// for (let i = 0; i < 100; i++) {
+//   context.fillStyle = Utils.hsl(Math.random() * 36 + i * 36, 100, 50);
+//   for (let j = 0; j < 100; j++) {
+//     context.fillRect(2700 - j * 30, j * 30 - i * 30, 30, 30);
+//   }
+// }
 
-for (let i = 0; i < 50; i++) {
-  context.fillStyle = Utils.hsl(Math.random() * 360, 100, 50);
-  for (let j = 0; j < 58; j++) {
-    context.fillRect(1707 - j * 30, j * 25 + i * 30, 25, 25);
-    context.fillRect(1707 - j * 30, j * 25 - i * 30, 25, 25);
+// kleuren
+let colors = ["magenta", "blue", "cyan", "green", "yellow", "orange", "red"];
+
+// Random start maken
+let startIndex = Math.floor(Math.random() * colors.length);
+// aantal diagonalen
+for (let i = 0; i < 100; i++) {
+  //kiest begin en zorgt vr herhaling
+  let colorIndex = (startIndex + i) % colors.length;
+  context.fillStyle = colors[colorIndex];
+  // hoeveel in 1 diagonaal
+  for (let j = 0; j < 100; j++) {
+    context.fillRect(2700 - j * 30, j * 30 - i * 30, 30, 30);
   }
 }
