3225370133e78d017a68d89ccdae62dc03582d34
Kiara Geuten <kiara.geuten@student.ehb.be>
2024-11-09 23:03:22 +0100

    kg gemaakt

--- milestones/generativedesign.js
+++ milestones/generativedesign.js
@@ -1,12 +1,7 @@
 import context from "../scripts/context.js";
 import * as Utils from "../scripts/utils.js";
 
-// for (let i = 0; i < 100; i++) {
-//   context.fillStyle = Utils.hsl(Math.random() * 36 + i * 36, 100, 50);
-//   for (let j = 0; j < 100; j++) {
-//     context.fillRect(2700 - j * 30, j * 30 - i * 30, 30, 30);
-//   }
-// }
+//background
 
 // kleuren
 let colors = ["magenta", "blue", "cyan", "green", "yellow", "orange", "red"];
@@ -22,4 +17,29 @@ for (let i = 0; i < 100; i++) {
   for (let j = 0; j < 100; j++) {
     context.fillRect(2700 - j * 30, j * 30 - i * 30, 30, 30);
   }
+}
+
+// KG();
+function KG() {
+  context.fillStyle = "white";
+  // vert lijnen; k,g
+  for (let x = 0; x < 15; x++) {
+    context.fillRect(600, 240 + x * 30, 30, 30);
+    context.fillRect(990, 240 + x * 30, 30, 30);
+  }
+  // lijn omhoog + lijn omlaag K
+  for (let x = 0; x < 8; x++) {
+    context.fillRect(600 + x * 30, 450 + x * 30, 30, 30);
+    context.fillRect(600 + x * 30, 450 - x * 30, 30, 30);
+  }
+
+  // hroizontale lijnen G + verticale halve van g
+  for (let x = 0; x < 7; x++) {
+    context.fillRect(990 + x * 30, 240, 30, 30);
+    context.fillRect(990 + x * 30, 660, 30, 30);
+    context.fillRect(1170, 660 - x * 30, 30, 30);
+  }
+  // horizon klein
+  context.fillRect(1140, 480, 30, 30);
+  context.fillRect(1110, 480, 30, 30);
 }
