e47beb7418ebc9b6c5e3e2aca87a6d1df03fc966
Kiara Geuten <kiara.geuten@student.ehb.be>
2024-12-17 19:25:30 +0100

    parameters added

--- milestones/generativedesign.js
+++ milestones/generativedesign.js
@@ -21,80 +21,78 @@ for (let i = 0; i < 100; i++) {
   }
 }
 
-//letter posities tov midden
-let heightLetters = 240;
-let middleX = width / 2;
-// offset wisselen vr verplaatsing
-let offsetK = middleX - 253.5;
-let offsetG = middleX + 46.5;
-let offsetE = middleX - 103.5;
-let offsetD = middleX - 493.5;
-let offsetV = middleX + 256.5;
-
-//teken kg
-function KG() {
-  context.fillStyle = "white";
-  // vert lijnen; k,g
-  for (let x = 0; x < 15; x++) {
-    context.fillRect(offsetK, heightLetters + x * size, size, size);
-    context.fillRect(offsetG, heightLetters + x * size, size, size);
-  }
-  // lijn omhoog + lijn omlaag K
-  for (let x = 0; x < 8; x++) {
-    context.fillRect(offsetK + x * size, 450 + x * size, size, size);
-    context.fillRect(offsetK + x * size, 450 - x * size, size, size);
-  }
-
-  // hroizontale lijnen G + verticale halve van g
-  for (let x = 0; x < 7; x++) {
-    context.fillRect(offsetG + x * size, heightLetters, size, size);
-    context.fillRect(offsetG + x * size, 660, size, size);
-    context.fillRect(middleX + 226.5, 660 - x * size, size, size);
-  }
-  // horizon klein
-  context.fillRect(middleX + 196.5, 480, size, size);
-}
-// teken dev
-function DEV() {
-  context.fillStyle = "white";
-  // verticalen
-  for (let x = 0; x < 15; x++) {
-    context.fillRect(offsetE, heightLetters + x * size, size, size);
-    context.fillRect(offsetD, heightLetters + x * size, size, size);
-  }
-  // korte ver D
-  for (let x = 0; x < 13; x++) {
-    context.fillRect(middleX - 253.5, 270 + x * size, size, size);
-  }
-  //vert V
-  for (let x = 0; x < 11; x++) {
-    context.fillRect(offsetV, heightLetters + x * size, size, size);
-    context.fillRect(offsetV + 240, heightLetters + x * size, size, size);
-  }
-  // horizontalen
-  for (let x = 0; x < 8; x++) {
-    //E
-    context.fillRect(offsetE + x * size, heightLetters, size, size);
-    context.fillRect(offsetE + x * size, 450, size, size);
-    context.fillRect(offsetE + x * size, 660, size, size);
-    //D
-    context.fillRect(offsetD + x * size, heightLetters, size, size);
-    context.fillRect(offsetD + x * size, 660, size, size);
-  }
-
-  // diag
-  for (let x = 0; x < 4; x++) {
-    context.fillRect(offsetV + 30 + x * size, 570 + x * size, size, size);
-    context.fillRect(offsetV + 240 - x * size, 540 + x * size, size, size);
-  }
-}
 // switch between dev en kg
 if (localStorage.getItem("refresh") === "KG") {
-  KG();
+  KG(150, 150);
   localStorage.setItem("refresh", "DEV");
 } else {
-  DEV();
+  DEV(300, 200);
   localStorage.setItem("refresh", "KG");
 }
 
-//parameters meegeven: func kg(x,y,size) -> kg(10,20)
+//teken kg
+function KG(Kposx, y) {
+  let Gposx = Kposx + size * 10;
+  context.fillStyle = "white";
+  // vert lijnen; k,g
+  for (let i = 0; i < 15; i++) {
+    context.fillRect(Kposx, y + i * size, size, size);
+    context.fillRect(Gposx, y + i * size, size, size);
+  }
+  // lijn omhoog + lijn omlaag K
+  for (let i = 0; i < 8; i++) {
+    context.fillRect(Kposx + i * size, y + 7 * size + i * size, size, size);
+    context.fillRect(Kposx + i * size, y + 7 * size - i * size, size, size);
+  }
+
+  // hroizontale lijnen G + verticale halve van g
+  for (let i = 0; i < 7; i++) {
+    context.fillRect(Gposx + i * size, y, size, size);
+    context.fillRect(Gposx + i * size, y + size * 14, size, size);
+    context.fillRect(Gposx + size * 6, y + size * 14 - i * size, size, size);
+  }
+
+  // horizon klein
+  context.fillRect(Gposx + size * 5, y + size * 8, size, size);
+}
+// teken dev
+function DEV(Dposx, y) {
+  let Epos = Dposx + size * 14;
+  let Vpos = Epos + size * 13;
+  context.fillStyle = "white";
+  // verticalen
+  for (let i = 0; i < 15; i++) {
+    context.fillRect(Epos, y + i * size, size, size);
+    context.fillRect(Dposx, y + i * size, size, size);
+  }
+  // korte ver D
+  for (let i = 0; i < 13; i++) {
+    context.fillRect(Dposx + 8 * size, y + size + i * size, size, size);
+  }
+  //vert V
+  for (let x = 0; x < 11; x++) {
+    context.fillRect(Vpos, y + x * size, size, size);
+    context.fillRect(Vpos + 240, y + x * size, size, size);
+  }
+  // horizontalen
+  for (let i = 0; i < 8; i++) {
+    //E
+    context.fillRect(Epos + i * size, y, size, size);
+    context.fillRect(Epos + i * size, y + size * 7, size, size);
+    context.fillRect(Epos + i * size, y + size * 14, size, size);
+    //D
+    context.fillRect(Dposx + i * size, y, size, size);
+    context.fillRect(Dposx + i * size, y + size * 14, size, size);
+  }
+
+  // diag
+  for (let i = 0; i < 5; i++) {
+    context.fillRect(Vpos + i * size, y + size * 10 + i * size, size, size);
+    context.fillRect(
+      Vpos + 8 * size - i * size,
+      y + size * 10 + i * size,
+      size,
+      size
+    );
+  }
+}
